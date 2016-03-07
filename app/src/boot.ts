7779d699748741cfee3faa0b857e17c5e5f90032
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-06 20:58:00 -0500

    Bootstrapped app

---
+++ app/src/boot.ts
@@ -0,0 +1,7 @@
+/// <reference path="_all.ts" />
+'use strict';
+
+module ContactManagerApp {
+  angular.module('contactManagerApp', ['ngMaterial'])
+    .controller('mainController', MainController);
+}
---
+++ app/src/controllers/mainController.ts
@@ -0,0 +1,14 @@
+/// <reference path="../_all.ts" />
+'use strict';
+
+module ContactManagerApp {
+  export class MainController {
+    static $inject = [];
+
+    constructor() {
+
+    }
+    
+    message: string = "Hello from controller";
+  }
+}
